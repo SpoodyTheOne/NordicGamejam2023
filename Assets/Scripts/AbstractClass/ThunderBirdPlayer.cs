c2cdc40452872cac912c57407230e5d6ac50997a
seb.nor.san <85798227+sebnorsan@users.noreply.github.com>
2023-04-15 00:34:09 +0200

    afaefaefo

--- Assets/Scripts/AbstractClass/ThunderBirdPlayer.cs
+++ Assets/Scripts/AbstractClass/ThunderBirdPlayer.cs
@@ -12,21 +12,38 @@ public class ThunderBirdPlayer : Player
     [SerializeField] private Image timeForFly;
     private bool lerpTime;
 
-    float timeElapsed;
-    float birdSpeed = 4;
+    [SerializeField] GameObject[] fx;
+    private GameObject effect = null;
 
+    float timeElapsed;
+    float birdSpeed;
     public override void Awake()
     {
         base.Awake();
 
-
+        BirdFormDeactivate();
     }
     public override void Update()
     {
         base.Update();
 
-        if (Input.GetMouseButtonDown(1))
+        Debug.Log(speed);
+
+        speed = Mathf.Lerp(speed, 4f, timeElapsed);
+
+        if (speed > 4.0001)
         {
+            timeElapsed += Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(mousePos.x, mousePos.y), speed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(1) && !birdForm)
+        {
+            effect = Instantiate(fx[0], gameObject.transform.position, Quaternion.identity);
+            Destroy(effect, 1.2f);
+
+            effect.transform.parent = playerForms[1].transform;
+
             timeElapsed = 0;
             birdForm = true;
         }
@@ -44,7 +61,7 @@ public class ThunderBirdPlayer : Player
     }
 
     private void BirdFormActivate()
-    {   
+    {
         if (timeElapsed < flyTime)
         {
             float valueToLerp = Mathf.Lerp(0, 1, timeElapsed / flyTime);
@@ -52,21 +69,25 @@ public class ThunderBirdPlayer : Player
             timeForFly.fillAmount = timeElapsed;
         }
 
+        speed = Mathf.Lerp(speed, 16f, timeElapsed);
+
         playerForms[1].SetActive(true);
         playerForms[0].SetActive(false);
 
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(mousePos.x, mousePos.y), birdSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(mousePos.x, mousePos.y), speed * Time.deltaTime);
 
         if (timeElapsed > flyTime)
             BirdFormDeactivate();
     }
     private void BirdFormDeactivate()
     {
+        if (effect != null)
+            effect.transform.parent = null;
+
+        timeElapsed = 0;
         birdForm = false;
 
         playerForms[0].SetActive(true);
         playerForms[1].SetActive(false);
-
-        speed = 4;
     }
 }
