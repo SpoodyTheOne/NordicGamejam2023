ea4e94d55227550e935d2380d9980018a1a1f610
SpoodyTheOne <sxsgamer@gmail.com>
2023-04-15 22:38:27 +0200

    eowurng

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> Rooms;
+
+    public GameObject StartRoom;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartRoom.GetComponent<RoomController>().NextWave();
     }
 
     // Update is called once per frame
