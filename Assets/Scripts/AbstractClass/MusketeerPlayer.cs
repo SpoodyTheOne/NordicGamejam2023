8e78a5a12d0c794efd15bc4e512aff5630f282c8
seb.nor.san <85798227+sebnorsan@users.noreply.github.com>
2023-04-15 21:48:51 +0200

    aaaaaaaaaaa

--- Assets/Scripts/AbstractClass/MusketeerPlayer.cs
+++ Assets/Scripts/AbstractClass/MusketeerPlayer.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class MusketeerPlayer : Player
 {
+    [SerializeField] private GameObject fx;
+    public override void Update()
+    {
+        base.Update();
 
+        if (Input.GetKeyDown(KeyCode.E))
+            Target();
+    }
+    private void Target()
+    {
+        var target = Instantiate(fx, mousePos, Quaternion.identity);
+    }
     public void StartPFX()
     {
         pfx.Play();
