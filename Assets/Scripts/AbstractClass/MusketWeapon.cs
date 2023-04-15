89323461b2fe36e0282b850b8b24ec023a20fbf8
seb.nor.san <85798227+sebnorsan@users.noreply.github.com>
2023-04-15 19:32:05 +0200

    afafa

--- Assets/Scripts/AbstractClass/MusketWeapon.cs
+++ Assets/Scripts/AbstractClass/MusketWeapon.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class MusketWeapon : Weapon
 {
     private Animator anim;
-    [SerializeField] private ParticleSystem[] pfx;
+    [SerializeField] private ParticleSystem pfx;
     private bool canShoot = true;
 
     private void Awake()
@@ -20,29 +20,16 @@ public class MusketWeapon : Weapon
 
         if (Input.GetMouseButtonDown(0) && canShoot)
             Shoot();
-        if (Input.GetMouseButtonDown(1) && canShoot)
-            MassiveShot();
     }
 
     private void Shoot()
     {
         canShoot = false;
 
-        pfx[0].Play();
-        pfx[0].gameObject.GetComponent<AudioSource>().Play();
+        pfx.Play();
+        pfx.gameObject.GetComponent<AudioSource>().Play();
          
         anim.SetTrigger("Shoot");
-    }
-    private void MassiveShot()
-    {
-        canShoot = false;
-
-        pfx[1].Play();
-        pfx[1].gameObject.GetComponent<AudioSource>().Play();
-
-        CameraShaker.Instance.ShakeOnce(10f, 10f, 0f, .67f);
-
-        anim.SetTrigger("BigShoot");
     }
     public void CanShoot()
     {
@@ -55,15 +42,6 @@ public class MusketWeapon : Weapon
         {
             if (canShoot)
                 Shoot();
-        }
-    }
-
-    public void RightClickAbility(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
-            if (canShoot)
-                MassiveShot();
         }
     }
 }
