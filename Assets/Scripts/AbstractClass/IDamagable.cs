645b271b542d5682e966103adf31963bb6d6baf0
SpoodyTheOne <sxsgamer@gmail.com>
2023-04-15 12:09:32 +0200

    Added Comments to IDamagable.cs

--- Assets/Scripts/AbstractClass/IDamagable.cs
+++ Assets/Scripts/AbstractClass/IDamagable.cs
@@ -16,25 +16,31 @@ public abstract class IDamagable : MonoBehaviour
 
     public virtual bool TakeDamage(GameObject attacker, float Amount)
     {
+        // Check if we have iframes
         if (IFrameTime > Time.time)
-            return false;
+            return false; // Return false to indicate a non-successful hit
 
+        // Update IFrameTime so we have iframes for this long
         IFrameTime = Time.time + IFrameSeconds;
 
+        // Remove decimal numbers in case we want to use a hearts system instead
         float DamageAmount = Amount;
-
         if (UseHearts)
             DamageAmount = Mathf.Floor(Amount);
 
+        // Do the damage
         this._Health -= DamageAmount;
         
+        // Check if the gameobject has a BuffManager
         BuffManager buffManager = GetComponent<BuffManager>();
         if (buffManager)
-            buffManager.OnDamaged();
-
-        if (this.Health <= 0)
+            buffManager.OnDamaged(); // Trigger buff manager OnDamaged() event
+        
+        // Die if health is 0
+        if (this._Health <= 0)
             this.Die();
-
+    
+        // Return true to indicate a successful hit
         return true;
     }
 
@@ -42,7 +48,7 @@ public abstract class IDamagable : MonoBehaviour
     {       
         BuffManager buffManager = GetComponent<BuffManager>();
         if (buffManager)
-            buffManager.OnDeath();
+            buffManager.OnDeath(); // Trigger OnDeath() event if the gameobject has a BuffManager
 
         this._Dead = true; 
     }
