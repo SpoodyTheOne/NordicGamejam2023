0d7c681a623a111ff639bdfcb10c3d5ddb65b5fc
BertramBims <bertram.winter004@gmail.com>
2023-04-15 22:25:51 +0200

    gfbd tbh

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public Player[] players;
+    public TMP_Text[] texts;
+
+    public void Update()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+
+        }
+    }
+}
